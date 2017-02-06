035501d02d80da82d2752b0c9f1a2d15b1a822da
Thomas Collins <tomascollins@gmail.com>
2017-02-06 15:54:26 -0800

    remove containers not being used

--- src/containers/RecipeContainer.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-import RecipeList from '../components/RecipeList';
-import { deleteRecipe } from '../actions'
-
-const mapStateToProps = (state) => ({ recipes: state.recipes });
-
-const RecipeContainer = connect(mapStateToProps, deleteRecipe)(RecipeList);
-
-export default RecipeContainer;
